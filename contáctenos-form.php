cde3f73c477fa497b2c6e7bc30158c8d5aff34d1
Kacy McKibben <kacy.mckibben@gmail.com>
2016-12-04 22:56:23 -0800

    finished updating contact forms on index and inicio
    also added social media links

---
+++ contáctenos-form.php
@@ -0,0 +1,30 @@
+<?php
+if (!isset($_POST['submit'])) {
+   echo "<h1>Error</h1>";
+   echo "<p>Accessing this page directly is not allowed.</p>";
+   exit();
+}
+if (isset($_POST['submit'])) {
+  $to = "kacy.mckibben@gmail.com";
+  $email = $_POST['email'];
+  $subject = "Form submission";
+  $subject2 = "Thank you for contacting L.A. Rights!";
+  $message = $_POST['message'];
+  $message2 = "Thank you for contacting L.A. Rights! We will get back to you shortly. Here is a copy of your message: " . $_POST['message'];
+  $headers = "Reply-To: " . $_POST['email'];
+  mail($to,$subject,$message,$headers);
+  $mail_sent = mail($email,$subject2,$message2);
+}
+if ($mail_sent == true) { ?>
+  <script language="javascript" type="text/javascript">
+    alert('Thank you for contacting L.A. Rights! We will get back to you shortly.');
+    window.location = 'contact.html';
+  </script>
+  <?php } else { ?>
+  <script language="javascript" type="text/javascript">
+    alert('Your message was unable to be sent. Please try again or contact us via phone at 310-906-0946.');
+    window.location = 'contáctenos.html';
+    </script>
+  <?php
+}
+?>
